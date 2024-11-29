c3ab0c5079843c25eb5e7e766037fd8a145be3a7
yaaAYA25 <yowwawe@gmail.com>
2024-11-29 13:17:34 +0700

    Add files via upload

---
+++ ProjectUAS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uasPrak;
+
+/**
+ *
+ * @author USER
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+public class ProjectUAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
